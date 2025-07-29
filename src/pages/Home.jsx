d4599b5fe0207027a059f9eb5f5dab45033cfb86
g <diegocaucota@gmail.com>
2025-07-29 13:36:30 -0300

    add: api configuration key

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,7 +23,7 @@ export default function Home() {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {movies.length ? (
           movies.map((movie) => (
-            <MovieCard key={movie.imdbID} movie={movie} />
+            <MovieCard key={movie.id} movie={movie} />
           ))
         ) : (
           <p className="text-center col-span-full">No se encontraron resultados</p>
--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,20 @@
 const API_URL = import.meta.env.VITE_API_URL;
 const API_KEY = import.meta.env.VITE_API_KEY;
 
-export async function fetchPopularMovies() {
-  const res = await fetch(`${API_URL}/movie/popular?api_key=${API_KEY}&language=es-ES`);
-  if (!res.ok) throw new Error('Error al obtener los datos de la pelicula'); 
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0Mzk1YmM4NDNiZDc5YzZjMjBiZjBlZjIwMjEzY2Y3OSIsIm5iZiI6MTYyNDc2MTI1My4wNDEsInN1YiI6IjYwZDdlM2E1Y2M5NjgzMDA3NmQxNWY2YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LBA7gJ9VZku7igohtcXP4MahP8gFwazdY-f9AEXf8vs'
+  }
+};
+
+export async function fetchMovies() {
+  // api_key=${API_KEY}
+    // /movie/popular?language=en-US&page=1', options
+  const res = await fetch(`${API_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`, options);
+  if (!res.ok) throw new Error('Error al obtener los datos de la pelicula');
+  console.log(res) 
   const data = await res.json();
   return data.results;
 }
