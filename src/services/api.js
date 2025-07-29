616d8e9557430b781a4e712c3483702b5e17f5df
g <diegocaucota@gmail.com>
2025-07-29 14:17:01 -0300

    add: components MovieDetail

--- src/services/api.js
+++ src/services/api.js
@@ -1,20 +1,22 @@
 const API_URL = import.meta.env.VITE_API_URL;
 const API_KEY = import.meta.env.VITE_API_KEY;
 
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0Mzk1YmM4NDNiZDc5YzZjMjBiZjBlZjIwMjEzY2Y3OSIsIm5iZiI6MTYyNDc2MTI1My4wNDEsInN1YiI6IjYwZDdlM2E1Y2M5NjgzMDA3NmQxNWY2YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LBA7gJ9VZku7igohtcXP4MahP8gFwazdY-f9AEXf8vs'
-  }
+/**
+ * Busca una película por ID
+ * @param {string} imdbID
+ */
+export const fetchMovieById = async (imdbID) => {
+  const response = await fetch(`${API_URL}?i=${imdbID}&apikey=${API_KEY}`);
+  const data = await response.json();
+  return data;
 };
 
-export async function fetchMovies() {
-  // api_key=${API_KEY}
-    // /movie/popular?language=en-US&page=1', options
-  const res = await fetch(`${API_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`, options);
-  if (!res.ok) throw new Error('Error al obtener los datos de la pelicula');
-  console.log(res) 
-  const data = await res.json();
-  return data.results;
-}
+/**
+ * Buscar películas por nombre
+ * (esto lo dejamos por si querés mantener búsqueda por título)
+ */
+export const searchMovies = async (title = "batman") => {
+  const response = await fetch(`${API_URL}?s=${title}&apikey=${API_KEY}`);
+  const data = await response.json();
+  return data.Search || [];
+};
